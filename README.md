7a696852ed9499d0c46abddfe4555e77e4266f0f
nchamo <nicolas@chamo.com.ar>
2020-02-13 16:59:09 -0300

    Update README and add tooling

--- README.md
+++ README.md
@@ -46,3 +46,16 @@ To stop your Catalyst, you can run:
 ## FAQ
 ### 1. How are TLS certificates managed?
 We are using [Let's Encrypt](https://letsencrypt.org/) for certificates. We automatically generate the certificates the first time you start your server. We will also handle the renewal of the server for you!
+
+### 2. When issuing a certificate, if fails the first time, the script won't try to issue a new one.
+One of the steps in issuing a certificate is the creation of auto signed certs. The script checks if there are any certs already created. If they exist, then no new certificates are created.
+
+To force a certificate regeneration you will need to change the `REGENERATE` entry on the `.env` file to `1` and then execute the script again.
+
+**IT'S IMPORTANT** to roll back this value to `0`. If you don't, a new certificate will be issued on each run and you will hit the Let's Encript ratio limit. It this happens, your domain will be banned.
+
+### 3. How many certs I can issue?
+This is tied to Let's Encrypt ratio limit. To know about Let's Encrypt ratio limit look [here](https://letsencrypt.org/docs/staging-environment/)
+
+### 4. How can I can check the amount of certificates I already issued?
+You can take a look on this [site](https://crt.sh/), entering the domain you want to check.
---
+++ tooling/resync.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+printMessage () {
+    Type=$1
+    case ${Type} in
+      ok) echo -e "[\e[92m OK \e[39m]" ;;
+      failed) echo -e "[\e[91m FAILED \e[39m]" ;;
+      *) echo "";;
+    esac
+}
+
+# Make sure env file exists
+if ! [ -f ".env" ]; then
+  echo -n "Error: .env does not exist" >&2
+  printMessage failed
+  exit 1
+else
+  # If it exists, load it
+  source ".env"
+fi
+
+# Make sure that the storage exists
+echo -n "## Checking if storage is configured..."
+if test -d ${CONTENT_SERVER_STORAGE}; then
+    printMessage ok
+else
+    echo -n "Failed to check the storage."
+    printMessage failed
+    exit 1
+fi
+
+# Set vars
+IMMUTABLE=${CONTENT_SERVER_STORAGE}/history/immutableHistory.log
+IMMUTABLE_OLD=${CONTENT_SERVER_STORAGE}/history/immutableHistory-old.log
+TEMP=${CONTENT_SERVER_STORAGE}/history/tempHistory.log
+TEMP_OLD=${CONTENT_SERVER_STORAGE}/history/tempHistory-old.log
+
+# Check if there is an immutable history log or not
+if test -f ${IMMUTABLE}; then
+    # Stop the content server
+    docker-compose rm -fs content-server
+
+    # Move all immutable history into the temp history
+    # Old temp and immutable logs will be stored with a '-old' suffix
+    cp ${TEMP} ${TEMP_OLD}
+    ((tac 2> /dev/null || tail -r) < ${IMMUTABLE}) >> ${TEMP}
+    mv ${IMMUTABLE} ${IMMUTABLE_OLD}
+
+    # Start the content server again
+    docker-compose up --force-recreate -d content-server
+
+    echo "DONE!"
+else
+    echo "No immutable history found, so no resync is necessary"
+fi
