99ada72848fb9a93ba4d6ecbd68fcac9f273d93e
Alejandro Drabenche <alejandro@decentraland.org>
2020-02-14 12:03:02 -0300

    Added verbosity to stop script

--- stop.sh
+++ stop.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
+
+printMessage () {
+    Type=$1
+    case ${Type} in
+      ok) echo -e "[\e[92m OK \e[39m]" ;;
+      failed) echo -e "[\e[91m FAILED \e[39m]" ;;
+      *) echo "";;
+    esac
+}
+
+echo "## Stopping catalyst node..."
 docker-compose stop
+
+##C hecking if everything went Ok
+###
+if test $? -ne 0; then
+  echo -n "Failed to stop catalyst node "
+  printMessage failed
+  exit 1
+fi
+echo -n "## node stopped Ok... "
+printMessage ok
