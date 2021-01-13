b9c87e7c1918ae7d324226b3e6192b7f8a8ee4fd
Agustin Mendez <github@menduz.com>
2021-01-13 13:53:18 -0300

    fix: make crontab compatible with old versions of git (apt)

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update the catalyst-owner version using the current branch
-git pull origin "$(git branch --show-current)"
+git pull origin "$(git rev-parse --abbrev-ref HEAD)"
 
 # Update the version if we have messages in the SQS
 bash ./receive-sqs.sh
