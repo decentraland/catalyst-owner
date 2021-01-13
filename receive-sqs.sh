cde38bb888007bcbdfc96a3f2cced09d0599b1e2
Agustin Mendez <github@menduz.com>
2021-01-13 17:55:18 -0300

    chore: provide SQS_QUEUE_NAME instead of SQS_URL

--- receive-sqs.sh
+++ receive-sqs.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-# Update the version if we have messages in the SQS
-if [ -n "${SQS_URL}" ]; then
-  MSG=$(aws sqs receive-message --queue-url "$SQS_URL");
-  if [ -n "$MSG" ]
-  then
-    DOCKER_TAG=$(echo "$MSG" | jq -r '.Messages[0].Body' | jq -r .Message | jq -r .version);
-    export DOCKER_TAG
+# reads a message from an SQS named SQS_QUEUE_NAME and performs the update based on the .version field of the message
 
-    bash ./init.sh
+if [ -n "${SQS_QUEUE_NAME}" ]; then
+  SQS_URL=$(aws sqs get-queue-url --queue-name "${SQS_QUEUE_NAME}" | jq -r .QueueUrl)
 
-    EXIT_CODE=$?
+  # Update the version if we have messages in the SQS
+  if [ -n "${SQS_URL}" ]; then
+    MSG=$(aws sqs receive-message --queue-url "$SQS_URL");
 
-    # Only remove the message from the queue if the update was successful
-    if [ $EXIT_CODE -eq 0 ]; then
-      echo "$MSG" | jq -r '.Messages[0] | .ReceiptHandle' | (xargs -I {} aws sqs delete-message --queue-url "$SQS_URL" --receipt-handle {});
+    if [ -n "$MSG" ]; then
+      DOCKER_TAG=$(echo "$MSG" | jq -r '.Messages[0].Body' | jq -r .Message | jq -r .version);
+      export DOCKER_TAG
+
+      bash ./init.sh
+
+      EXIT_CODE=$?
+
+      # Only remove the message from the queue if the update was successful
+      if [ $EXIT_CODE -eq 0 ]; then
+        echo "$MSG" | jq -r '.Messages[0] | .ReceiptHandle' | (xargs -I {} aws sqs delete-message --queue-url "$SQS_URL" --receipt-handle {});
+      else
+        echo "[ERROR] consuming message $MSG"
+        exit 1
+      fi
     else
-      echo "Error consuming message $MSG"
+      echo 'No message was received'
     fi
   else
-    echo 'No message was received'
+    echo 'No SQS_URL is set'
   fi
 else
-  echo 'No SQS_URL is set'
+  echo 'No SQS_QUEUE_NAME is set'
 fi
