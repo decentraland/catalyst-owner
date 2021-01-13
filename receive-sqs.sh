3faafacea1af04578d18745dd582d48c5fcf5909
Agustin Mendez <github@menduz.com>
2021-01-13 14:25:58 -0300

    fix: use message body instead of attributes

--- receive-sqs.sh
+++ receive-sqs.sh
@@ -5,7 +5,7 @@ if [ -n "${SQS_URL}" ]; then
   MSG=$(aws sqs receive-message --queue-url "$SQS_URL");
   if [ -n "$MSG" ]
   then
-    DOCKER_TAG=$(echo "$MSG" | jq -r '.Messages[] | .Body' | jq -r '.MessageAttributes.version.Values');
+    DOCKER_TAG=$(echo "$MSG" | jq -r '.Messages[0].Body' | jq -r .Message | jq -r .version);
     export DOCKER_TAG
 
     bash ./init.sh
@@ -14,7 +14,7 @@ if [ -n "${SQS_URL}" ]; then
 
     # Only remove the message from the queue if the update was successful
     if [ $EXIT_CODE -eq 0 ]; then
-      echo "$MSG" | jq -r '.Messages[] | .ReceiptHandle' | (xargs -I {} aws sqs delete-message --queue-url "$SQS_URL" --receipt-handle {});
+      echo "$MSG" | jq -r '.Messages[0] | .ReceiptHandle' | (xargs -I {} aws sqs delete-message --queue-url "$SQS_URL" --receipt-handle {});
     else
       echo "Error consuming message $MSG"
     fi
