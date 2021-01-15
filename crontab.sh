503c947a46150e4f11b66bd7faf34a22849eef5b
Agustin Mendez <github@menduz.com>
2021-01-15 14:19:38 -0300

    chore: execute ./init.sh after pulling new version of the catalyst-owner

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Update the catalyst-owner version using the current branch
-git pull origin "$(git rev-parse --abbrev-ref HEAD)" --quiet
+git config pull.rebase true
+git config rebase.autoStash true
 
-# Update the version if we have messages in the SQS
-bash ./receive-sqs.sh
+# select the provided GIT_CATALYST_OWNER_BRANCH or fallback to current_branch
+branch=${GIT_CATALYST_OWNER_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+
+# Update the catalyst-owner version using the current branch
+git pull origin "$branch" | grep -o 'Already up to date.'
+isUpToDate=$?
+
+if [ $isUpToDate -eq 0 ]; then
+  # if it is not up to date, reinit the catalyst to apply changes
+  bash ./init.sh
+else
+  # update the version if we have messages in the SQS
+  bash ./receive-sqs.sh
+fi
