2d5e96394542c70c9af900e647fa4f3b487099b5
Agustin Mendez <github@menduz.com>
2021-01-16 08:16:37 -0300

    chore: configure mounting point

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mkdir -p /opt/ebs
+source .env
+
+set -u # break on unbound variables
+set -x # verbose
+
+mkdir -p "${CONTENT_SERVER_STORAGE}"
 
 if ! fsck.ext4 -n /dev/xvdf; then
   mkfs.ext4 /dev/xvdf
@@ -8,13 +13,13 @@ else
   resize2fs /dev/xvdf
 fi
 
-until mount /dev/xvdf /opt/ebs; do
+until mount /dev/xvdf "${CONTENT_SERVER_STORAGE}"; do
   echo "Failed mounting volume; retrying in 10 seconds..."
   sleep 10
 done
 
 echo "Setting up automatic mounting of EBS volume..."
-blkid | grep xvdf | awk '{print $2}' | sed s/\"//g | awk '{print $1" /opt/ebs  xfs  defaults,nofail  0  2"}' | cat >> /etc/fstab
+blkid | grep xvdf | awk '{print $2}' | sed s/\"//g | awk '{print $1" "'$CONTENT_SERVER_STORAGE'"  xfs  defaults,nofail  0  2"}' | cat >> /etc/fstab
 
 echo "Initializing catalyst..."
 
