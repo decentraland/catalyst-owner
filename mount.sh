8f72ca199d07d94afa17a9770a7b0aca8ebd0392
Agustin Mendez <github@menduz.com>
2021-01-17 10:32:32 -0300

    chore: harden automatic mounting point for attached volumes

--- mount.sh
+++ mount.sh
@@ -19,12 +19,42 @@ if [ "$MOUNT_DISK" -ne "" ]; then
     resize2fs "$MOUNT_DISK"
   fi
 
-  until mount | grep /opt/ebs; do
-    echo "Tryint to mount ${CONTENT_SERVER_STORAGE}.."
+  echo "Tryint to mount $MOUNT_DISK to ${CONTENT_SERVER_STORAGE}.."
+  until mount | grep "${CONTENT_SERVER_STORAGE}"; do
     mount "$MOUNT_DISK" "${CONTENT_SERVER_STORAGE}";
     sleep 10
   done
 
-  echo "Setting up automatic mounting of EBS volume..."
-  blkid | grep "$MOUNT_DISK" | awk '{print $2}' | sed s/\"//g | awk '{print $1" '$CONTENT_SERVER_STORAGE'  xfs  defaults,nofail  0  2"}' | cat >> /etc/fstab
+  backupFile="/etc/fstab.$(date +%s)"
+
+  # find the mounted disk in fstab
+  cat /etc/fstab | grep -o "$MOUNT_DISK"
+
+  if [ $? -ne 0 ]; then
+    echo "Configuring fstab"
+
+    echo "Backing up to $backupFile"
+    cp /etc/fstab $backupFile
+
+    echo "Setting up automatic mounting of EBS volume..."
+    blkid \
+    | grep "$MOUNT_DISK" \
+    | awk '{print $2}' \
+    | sed s/\"//g \
+    | awk '{print $1" '$CONTENT_SERVER_STORAGE'  xfs  defaults  0  2"}' \
+    | cat >> /etc/fstab
+
+    diff $backupFile /etc/fstab
+
+    # unmount
+    umount "$MOUNT_DISK"
+    mount -a
+
+    # this should work
+    mount | grep "${CONTENT_SERVER_STORAGE}"
+
+    if [ $? -ne 0 ]; then
+      echo 'ERROR it was not possible to configure automatic mounting of the disk'
+    fi
+  fi
 fi
