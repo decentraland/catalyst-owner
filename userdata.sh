cf762b64cc16d454a9d3ffaabbfac9a5c5bd8fa2
Agustin Mendez <github@menduz.com>
2021-01-17 12:21:58 -0300

    chore: make apt less noisy

--- userdata.sh
+++ userdata.sh
@@ -4,29 +4,29 @@ set -x
 set -v # verbose
 set -u # no unbound variables
 
-apt-get update
-apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+apt-get -qq update
+apt-get -qq install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository -qq "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 # try to update couple of times
-apt-get update || sleep 5 && apt-get update || sleep 5 && apt-get update || sleep 5 && apt-get update || sleep 5 && apt-get update
-apt-get upgrade -y
+apt-get -qq update || sleep 5 && apt-get update || sleep 5 && apt-get update || sleep 5 && apt-get update || sleep 5 && apt-get update
+apt-get -qq upgrade -y
 
 # install docker and utils
 
 until docker --version; do
-  apt-get install docker-ce -y
+  apt-get -qq install docker-ce -y
   sleep 1
 done
 
 until jq --version; do
-  apt-get install jq -y
+  apt-get -qq install jq -y
   sleep 1
 done
 
 until aws --version; do
-  apt-get install awscli -y
+  apt-get -qq install awscli -y
   sleep 1
 done
 
