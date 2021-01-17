b4abe8d2fa85fce728eb97c2b92cd13dab07a264
Agustin Mendez <github@menduz.com>
2021-01-17 12:23:42 -0300

    fix: add-apt-repository doesnt accept -qq

--- userdata.sh
+++ userdata.sh
@@ -7,7 +7,7 @@ set -u # no unbound variables
 apt-get -qq update
 apt-get -qq install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository -qq "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 # try to update couple of times
 apt-get -qq update || sleep 5 && apt-get update || sleep 5 && apt-get update || sleep 5 && apt-get update || sleep 5 && apt-get update
