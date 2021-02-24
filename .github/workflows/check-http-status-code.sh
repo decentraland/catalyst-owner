db05fe06c043b41269ff0fb1555067a2dc285881
Agustin Mendez <github@menduz.com>
2021-02-24 09:28:04 -0300

    wip

--- .github/workflows/check-http-status-code.sh
+++ .github/workflows/check-http-status-code.sh
@@ -4,7 +4,10 @@ URL=$1; shift
 EXPECTED_STATUS=$1; shift
 
 STATUSCODE=$(curl --silent -v $* --output /dev/stderr --write-out "%{http_code}" "$URL")
-
+if [ $? -ne 0 ]; then
+  echo "❌ Curl failed"
+  exit 1
+fi
 if  test "$STATUSCODE" -ne $EXPECTED_STATUS; then
   echo "❌ Status code $STATUSCODE != $EXPECTED_STATUS"
   exit 1
