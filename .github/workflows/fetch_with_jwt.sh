768f1754eef4d045a8637a1ccd742c97b86ad465
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-06-17 16:40:51 -0300

    add debugging info

--- .github/workflows/fetch_with_jwt.sh
+++ .github/workflows/fetch_with_jwt.sh
@@ -6,31 +6,22 @@ URL=$1; shift
 EXPECTED_STATUS=$1; shift
 
 # Get challenge
-
-curl $CHALLENGE_URL 2>/dev/null > challenge_response.json
-CHALLENGE_COMPLEXITY=$(cat challenge_response.json | jq .complexity)
-CHALLENGE=$(cat challenge_response.json | jq .challenge)
+GIVEN_CHALLENGE=$(curl $CHALLENGE_URL 2>/dev/null)
+echo "Obtained challenge: ${GIVEN_CHALLENGE}"
+CHALLENGE_COMPLEXITY=$(echo ${GIVEN_CHALLENGE}| jq .complexity)
+CHALLENGE=$(echo ${GIVEN_CHALLENGE} | jq .challenge)
 
 # Solve Challenge
 CHALLENGE_NONCE=$(node ./.github/workflows/solveChallenge.js $CHALLENGE $CHALLENGE_COMPLEXITY)
 
-
 # Get JWT
-curl -X POST $CHALLENGE_URL 2>/dev/null --header "Content-Type: application/json" --verbose  --data '{
-    "complexity": "${CHALLENGE_COMPLEXITY}",
-    "challenge": "${CHALLENGE}",
-    "nonce":
-      "${CHALLENGE_NONCE}"
-  }' > jwt_response.json
-echo 'Aca llego: {
-    "complexity": "${CHALLENGE_COMPLEXITY}",
-    "challenge": "${CHALLENGE}",
-    "nonce":
-      "${CHALLENGE_NONCE}"
-  }'
-JWT=$(cat jwt_response.json | jq .jwt)
+CHALLENGE_BODY=$(echo "{\"complexity\": ${CHALLENGE_COMPLEXITY}, \"challenge\": ${CHALLENGE}, \"nonce\": \"${CHALLENGE_NONCE}\"}")
+echo "Sending challenge: ${CHALLENGE_BODY}"
+RESPONSE=$(curl -X POST $CHALLENGE_URL 2>/dev/null --header "Content-Type: application/json" --verbose  -d "$CHALLENGE_BODY" )
+echo "Response from pow auth server: ${RESPONSE}"
+JWT=$( echo ${RESPONSE}| jq .jwt)
 
-
+# Make the request
 STATUS_CODE=$(curl --insecure --silent -v $* --output /dev/stderr --write-out "%{http_code}"  -H "Cookie: JWT=${JWT}" "$URL")
 
 if [ $? -ne 0 ]; then
