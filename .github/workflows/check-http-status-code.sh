66c7969b47aeb066c596ec9fd4f98c41ca2960db
Agustina <agusaldasoro@users.noreply.github.com>
2021-06-22 11:54:26 -0300

    Update check-http-status-code.sh

--- .github/workflows/check-http-status-code.sh
+++ .github/workflows/check-http-status-code.sh
@@ -7,10 +7,6 @@ EXPECTED_STATUS=$1; shift
 # STATUSCODE=$(docker run --network container:nginx curlimages/curl --silent -v $* --output /dev/stderr --write-out "%{http_code}" "$URL")
 STATUSCODE=$(curl --insecure --silent -v $* --output /dev/stderr --write-out "%{http_code}" "$URL")
 
-if [ $? -ne 0 ]; then
-  echo "❌ Curl failed"
-  exit 1
-fi
 if  test "$STATUSCODE" -ne $EXPECTED_STATUS; then
   echo "❌ Status code $STATUSCODE != $EXPECTED_STATUS"
   exit 1
