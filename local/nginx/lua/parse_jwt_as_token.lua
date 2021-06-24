e667fa6b453018bc92cc4495f73120f1bbfb032e
Agustina <agusaldasoro@users.noreply.github.com>
2021-06-24 14:37:40 -0300

    Use Openresty with lua to configure JWT Rate Limit  (#65)
    + Expose Pow Auth Server

---
+++ local/nginx/lua/parse_jwt_as_token.lua
@@ -0,0 +1,10 @@
+local jwt = require "resty.jwt"
+
+local jwt_token = ngx.var.cookie_JWT
+local jwt_obj = jwt:verify(ngx.var.secret, jwt_token)
+
+if (jwt_obj.valid and jwt_obj.verified) then
+  return jwt_obj.payload.nonce
+else
+  return ""
+end
