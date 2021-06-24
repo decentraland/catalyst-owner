5fbfde2e4f914c49934428ab781ea7e401a42253
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-06-23 22:02:07 -0300

    Move to different files

--- local/nginx/lua/parse_jwt_as_token.lua
+++ local/nginx/lua/parse_jwt_as_token.lua
@@ -1,13 +1,10 @@
 local jwt = require "resty.jwt"
 
-local basePath = ngx.var.jwt_base_path
-local path = ngx.arg[1]
-
 local jwt_token = ngx.var.cookie_JWT
 local jwt_obj = jwt:verify(ngx.var.secret, jwt_token)
 
 if (jwt_obj.valid and jwt_obj.verified) then
-  return basePath .. path .. jwt_obj.payload.nonce
+  return jwt_obj.payload.nonce
 else
   return ""
 end
