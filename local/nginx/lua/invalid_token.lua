4c865f8cecef933ee014e4a3583c25fa4e5fc01b
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-07-08 13:49:51 -0300

    Return empty when no present

--- local/nginx/lua/invalid_token.lua
+++ local/nginx/lua/invalid_token.lua
@@ -1,8 +1,11 @@
 local jwt = require "resty.jwt"
 
 local jwt_token = ngx.var.cookie_JWT
-local jwt_obj = jwt:verify(ngx.var.secret, jwt_token)
+if (jwt_token == nil or jwt_token == "") then
+    return ""
+end
 
+local jwt_obj = jwt:verify(ngx.var.secret, jwt_token)
 if (jwt_obj.valid and jwt_obj.verified) then
     return ""
 else
