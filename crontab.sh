ef11abdf6d2ff6888a7622ea21023bb83b75de02
Matias Pentreath <pentreathm@decentraland.org>
2026-09-10 11:51:27 -0300

    fix: avoid reinitializing on every cron run

--- crontab.sh
+++ crontab.sh
@@ -16,7 +16,7 @@ branch=${GIT_CATALYST_OWNER_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 git pull origin "$branch" | grep -o 'Already up to date.'
 isUpToDate=$?
 
-if [ $isUpToDate -eq 0 ]; then
+if [ $isUpToDate -ne 0 ]; then
   # if it is not up to date, reinit the catalyst to apply changes
   bash ./init.sh
 else
